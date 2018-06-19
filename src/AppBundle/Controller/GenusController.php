41e163d301f13b92c38983c99cbd8042ced3c714
VladXII <PobereznichenkoVladimir@gmail.com>
2018-06-19 18:39:03 +0300

    First commit

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,6 +11,39 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        $genus->setSubFamily('Octodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>Genus created!!!</body></html>');
+    }
+
+    /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $genuses = $em->getRepository('AppBundle:Genus')->findAll();
+
+        return $this->render(
+            'genus/list.html.twig',
+            [
+                'genuses' => $genuses,
+            ]
+        );
+    }
+
     /**
      * @Route("/genus/{genusName}")
      */
@@ -31,10 +65,13 @@ class GenusController extends Controller
         $this->get('logger')
             ->info('Showing genus: '.$genusName);
 
-        return $this->render('genus/show.html.twig', array(
-            'name' => $genusName,
-            'funFact' => $funFact,
-        ));
+        return $this->render(
+            'genus/show.html.twig',
+            array(
+                'name' => $genusName,
+                'funFact' => $funFact,
+            )
+        );
     }
 
     /**
@@ -44,12 +81,30 @@ class GenusController extends Controller
     public function getNotesAction($genusName)
     {
         $notes = [
-            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
-            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
-            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+            [
+                'id' => 1,
+                'username' => 'AquaPelham',
+                'avatarUri' => '/images/leanna.jpeg',
+                'note' => 'Octopus asked me a riddle, outsmarted me',
+                'date' => 'Dec. 10, 2015',
+            ],
+            [
+                'id' => 2,
+                'username' => 'AquaWeaver',
+                'avatarUri' => '/images/ryan.jpeg',
+                'note' => 'I counted 8 legs... as they wrapped around me',
+                'date' => 'Dec. 1, 2015',
+            ],
+            [
+                'id' => 3,
+                'username' => 'AquaPelham',
+                'avatarUri' => '/images/leanna.jpeg',
+                'note' => 'Inked!',
+                'date' => 'Aug. 20, 2015',
+            ],
         ];
         $data = [
-            'notes' => $notes
+            'notes' => $notes,
         ];
 
         return new JsonResponse($data);
